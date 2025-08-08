a37eb325f2f5ffbdb34daaba616ff2927e4377b7
Sheetal Chouhan <sheetalchouhan@Sheetals-MacBook-Pro.local>
2025-08-08 21:21:49 +0530

    fix: resolved cors issue

--- model/user.model.js
+++ model/user.model.js
@@ -6,8 +6,6 @@ const userSchema = new mongoose.Schema(
     number: { type: Number, required: true, uniquie : true },
     email: { type: String, required: true , uniquie : true },
     password: { type: String, required: true }
-    
-
 },
 {
   timestamps : true
--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const cors = require('cors');
+
+
+dotenv.config();
 
 
 const hotelDataAddedToDBRouter = require("./routes/dataimport.router");
@@ -18,6 +23,7 @@ const connectDB = require("./config/dbconfig");
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 connectDB();
 
